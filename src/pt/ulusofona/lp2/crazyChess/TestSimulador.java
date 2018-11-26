f6c87630d7b507f902112d3e0a823e9691b6969c
21703913 <a21703913@alunos.ulht.pt>
2018-11-26 16:18:17 +0000

    1º Teste JUnit

--- src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
+++ src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
@@ -1,5 +1,19 @@
+//Estes testes destinam-se ao ficheiro de exemplo dado.
 package pt.ulusofona.lp2.crazyChess;
 
-public class TestSimulador {
+import org.junit.Test;
 
+import java.io.File;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestSimulador {
+    @Test
+    public void test01processaJogada() {
+        Simulador simulador = new Simulador();
+        File ficheiro = new File("jogo.txt");
+        simulador.iniciaJogo(ficheiro);
+        boolean jogada  = simulador.processaJogada(1,0,1,1);
+        assertTrue(jogada);
+    }
 }

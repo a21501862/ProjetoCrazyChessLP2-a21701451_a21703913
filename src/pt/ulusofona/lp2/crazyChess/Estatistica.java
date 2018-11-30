d09b770249d307195f81b1f4f35bb8074ba99f56
Rafael Lemos <a21701451@alunos.ulht.pt>
2018-11-30 09:38:38 +0000

    Adicionada e implementada classe Estatistica.

---
+++ src/pt/ulusofona/lp2/crazyChess/Estatistica.java
@@ -0,0 +1,67 @@
+package pt.ulusofona.lp2.crazyChess;
+
+public class Estatistica {
+    int jogadasInvalidasPretas;
+    int jogadasInvalidasBrancas;
+    int jogadasValidasPretas;
+    int jogadasValidasBrancas;
+    int capturasPretas;
+    int capturasBrancas;
+
+    Estatistica(){
+        this.jogadasInvalidasPretas = 0;
+        this.jogadasInvalidasBrancas = 0;
+        this.jogadasValidasPretas = 0;
+        this.jogadasValidasBrancas = 0;
+        this.capturasPretas = 0;
+        this.capturasBrancas = 0;
+    }
+
+    public int getCapturasBrancas() {
+        return capturasBrancas;
+    }
+
+    public int getCapturasPretas() {
+        return capturasPretas;
+    }
+
+    public int getJogadasInvalidasBrancas() {
+        return jogadasInvalidasBrancas;
+    }
+
+    public int getJogadasInvalidasPretas() {
+        return jogadasInvalidasPretas;
+    }
+
+    public int getJogadasValidasBrancas() {
+        return jogadasValidasBrancas;
+    }
+
+    public int getJogadasValidasPretas() {
+        return jogadasValidasPretas;
+    }
+
+    public void adicionaJogadasInvalidasPretas(){
+        jogadasInvalidasPretas++;
+    }
+
+    public void adicionaJogadasInvalidasBrancas(){
+        jogadasInvalidasBrancas++;
+    }
+
+    public void adicionaJogadasValidasPretas(){
+        jogadasValidasPretas++;
+    }
+
+    public void adicionaJogadasValidasBrancas(){
+        jogadasValidasBrancas++;
+    }
+
+    public void capturarPretas(){
+        capturasPretas++;
+    }
+
+    public void capturarBrancas(){
+        capturasBrancas++;
+    }
+}

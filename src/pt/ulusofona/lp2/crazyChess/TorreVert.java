3b4759705146e60736735569abf9e3d037754758
Bruna Vieites <a21703913@alunos.ulht.pt>
2018-12-19 09:29:02 +0000

    Adicionada as classes TorreHor e TorreVert

---
+++ src/pt/ulusofona/lp2/crazyChess/TorreVert.java
@@ -0,0 +1,60 @@
+package pt.ulusofona.lp2.crazyChess;
+
+import java.util.List;
+
+public class TorreVert extends CrazyPiece {
+
+    TorreVert(int idPeca, int tipo, int idEquipa, String alcunha){
+        super(idPeca,tipo,idEquipa,alcunha);
+    }
+
+    @Override
+    public String getValorRelativo() {
+        return null;
+    }
+
+    @Override
+    public String getTipo() {
+        return null;
+    }
+
+    boolean movePeca(int xO, int yO, int xD, int yD, Estatistica estatisticas, List<CrazyPiece> pecasJogo, InfoJogo jogo) {
+        if (xD != xO){
+            return false;
+        }
+        CrazyPiece pecaParaRemover = null;
+        for (CrazyPiece peca : pecasJogo) {
+            if((peca.getY()>yO && peca.getY()<yD && peca.getX()==xO) || (peca.getY()<yO && peca.getY()>yD && peca.getX()==xO)){
+                if (idEquipa == 10) {
+                    estatisticas.adicionaJogadasInvalidasPretas();
+                } else {
+                    estatisticas.adicionaJogadasInvalidasBrancas();
+                }
+                return false;
+            }
+            if (peca.getIdEquipa() != idEquipa && peca.getX() == xD && peca.getY() == yD) {
+                pecaParaRemover = peca;
+                peca.capturar();
+                jogo.primeiraCapturaFeita();
+                jogo.resetTurnosSemCapturas();
+                if (idEquipa == 10) {
+                    jogo.decrementaPecasBrancas();
+                    estatisticas.capturarPretas();
+                } else {
+                    jogo.decrementaPecasPretas();
+                    estatisticas.capturarBrancas();
+                }
+            }
+            if (peca.getIdEquipa() == idEquipa && peca.getX() == xD && peca.getY() == yD) {
+                if (idEquipa == 10) {
+                    estatisticas.adicionaJogadasInvalidasPretas();
+                } else {
+                    estatisticas.adicionaJogadasInvalidasBrancas();
+                }
+                return false;
+            }
+        }
+        pecasJogo.remove(pecaParaRemover);
+        return true;
+    }
+}

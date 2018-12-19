0b34c710c884b7d006a0d714a8f3f0d90d869d2e
21703913 <a21703913@alunos.ulht.pt>
2018-12-19 09:31:48 +0000

    Adicionada a classe Rainha

---
+++ src/pt/ulusofona/lp2/crazyChess/Rainha.java
@@ -0,0 +1,52 @@
+package pt.ulusofona.lp2.crazyChess;
+
+import java.util.List;
+
+public class Rainha extends CrazyPiece {
+
+    Rainha(int idPeca, int tipo, int idEquipa, String alcunha){
+        super(idPeca,tipo,idEquipa,alcunha);
+    }
+
+    @Override
+    public String getValorRelativo() {
+        return null;
+    }
+
+    @Override
+    public String getTipo() {
+        return null;
+    }
+
+    boolean movePeca(int xO, int yO, int xD, int yD, Estatistica estatisticas, List<CrazyPiece> pecasJogo, InfoJogo jogo){
+        if(xD > xO + 5 || yD > yO + 5 || xD < xO - 5 || yD < yO - 5){
+            return false;
+        }
+        CrazyPiece pecaParaRemover = null;
+        for (CrazyPiece peca : pecasJogo) {
+            if (peca.getIdEquipa() != idEquipa && peca.getX() == xD && peca.getY() == yD && peca.getIdTipo() != idTipo) {
+                pecaParaRemover = peca;
+                peca.capturar();
+                jogo.primeiraCapturaFeita();
+                jogo.resetTurnosSemCapturas();
+                if (idEquipa == 10) {
+                    jogo.decrementaPecasBrancas();
+                    estatisticas.capturarPretas();
+                } else {
+                    jogo.decrementaPecasPretas();
+                    estatisticas.capturarBrancas();
+                }
+            }
+            if ((peca.getIdEquipa() == idEquipa || peca.getIdTipo() == idTipo) && peca.getX() == xD && peca.getY() == yD) {
+                if (idEquipa == 10) {
+                    estatisticas.adicionaJogadasInvalidasPretas();
+                } else {
+                    estatisticas.adicionaJogadasInvalidasBrancas();
+                }
+                return false;
+            }
+        }
+        pecasJogo.remove(pecaParaRemover);
+        return true;
+    }
+}
